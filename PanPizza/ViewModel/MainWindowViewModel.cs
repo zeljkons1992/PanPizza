73538d606c17cf8d7451ecc3b611be928386a704
zeljkons1992 <zeljkons1992@gmail.com>
2018-02-03 14:19:56 +0100

    Resen problem zamrzavanja prozora.
    Resen problem zamrzavanja prozora tako sto je kreirana metoda za slanje sms poruke, i kreiran thread koji tu metodu izvrsava.

--- PanPizza/ViewModel/MainWindowViewModel.cs
+++ PanPizza/ViewModel/MainWindowViewModel.cs
@@ -4,6 +4,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Input;
@@ -16,6 +17,8 @@ namespace PanPizza.ViewModel
     class MainWindowViewModel : ViewModelBase
     {
         MainWindow mainWindow;
+
+        public static double smsUkupnaCena = 0;
 
         #region Constructor
         public MainWindowViewModel(MainWindow mainOpen)
@@ -148,24 +151,20 @@ namespace PanPizza.ViewModel
                 sb.AppendLine("---------------------------------");
                 sb.AppendLine("Vas racun iznosi " + ukupnaCena + " rsd.");
 
+                smsUkupnaCena = ukupnaCena;
+
                 mainWindow.lblsms.Visibility = Visibility.Visible;
                 mainWindow.lblRacun.Visibility = Visibility.Visible;
 
                 mainWindow.lblRacun.Content = sb.ToString();
 
-                TwilioClient.Init("AC191d51f60bdfc6186ae168b8ff857d4f", "d683d30561422ad700a4c520b559ea53");
-                MessageResource.Create(
-                  to: new PhoneNumber("+381641810049"),
-                  from: new PhoneNumber("+18302242798"),
-                  body: "Vaš račun iznosi " + ukupnaCena + " rsd");
-
-                
-
                 mainWindow.ListBox.IsEnabled = false;
                 mainWindow.cmbxSize.IsEnabled = false;
                 mainWindow.btnRacun.IsEnabled = false;
                 mainWindow.btnNova.IsEnabled = true;
-                
+
+                Thread t1 = new Thread(() => slanjeSMS());
+                t1.Start();     
             }
      
             catch (Exception ex)
@@ -230,6 +229,14 @@ namespace PanPizza.ViewModel
         {
             return true;
         }
-
+       
+        private static void slanjeSMS()
+        {
+            TwilioClient.Init("AC191d51f60bdfc6186ae168b8ff857d4f", "d683d30561422ad700a4c520b559ea53");
+              MessageResource.Create(
+              to: new PhoneNumber("+381641810049"),
+              from: new PhoneNumber("+18302242798"),
+              body: "Vaš račun iznosi " + smsUkupnaCena + " rsd");
+        }
     }
 }
